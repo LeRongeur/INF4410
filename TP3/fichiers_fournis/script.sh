7f920d7f580026f382eae3a60616a303f97da836
LeRongeur <ulric.villeneuve@polymtl.ca>
2017-12-04 11:37:01 -0500

    Fin des implementations. Avec repartiteur de cahrge sur 2 serveurs est 2 fois plus rapides.

---
+++ TP3/fichiers_fournis/script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -v
+
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+curl http://172.15.111.99:8000?nom=Bob &
+curl http://172.15.111.99:8000?nom=Alice &
+curl http://172.15.111.99:8000?nom=Simon &
+curl http://172.15.111.99:8000?nom=Ulric &
+curl http://172.15.111.99:8000?nom=Meredith &
+
+wait
